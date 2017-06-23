9244462a7ae31ab29d36445f854e7f5db4fbe8d4
user_02 <user_02@mac-02deimac.local>
2017-06-23 15:49:45 +0800

    Initial Commit

---
+++ imeiFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imeiFinal
+//
+//  Created by user_02 on 2017/6/23.
+//  Copyright © 2017年 wendy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
